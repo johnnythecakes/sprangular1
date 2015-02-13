9afe8cd4b2dc5e5a0b6472a04905c85270d99dd7
johnnythecakes <1zachjohnson@gmail.com>
2015-02-12 18:03:19 -0800

    first commit

---
+++ app/assets/javascripts/ng-app/app.js
@@ -0,0 +1,20 @@
+var app = angular.module('spaApp',['ui.router']);
+
+app.config(function($stateProvider, $urlRouterProvider){
+	$urlRouterProvider.otherwise('/');
+
+	$stateProvider
+		.state('home', {
+			url: '/home',
+			template: '<h1>HOME VIEW for {{user}}</h1>',
+			controller: function($scope){$scope.user = "Zach"}
+			})
+		.state('about',{
+			url: '/about',
+			template: '<h1>ABOUT VIEW</h1><a ui-sref=".portfolio">portfolio link</a> <div ui-view></div>'
+			})
+			.state('about.portfolio',{
+				url: '/portfolio',
+				template: '<h2>portfolio view</h2>',
+			})
+});
